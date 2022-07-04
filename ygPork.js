1fc665137142c8e615922f2a270bbdaa2d38a9e5
jystwo <jystwo@hanmail.net>
2022-07-04 18:22:44 +0900

    7.4

--- ygPork.js
+++ ygPork.js
@@ -1,17 +1,23 @@
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
-
+console.log("A");
     for (var i = 0; i < reveals.length; i++) {
+
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
         var elementVisible = 50;
 
         if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
+            reveals[i].classList.add("active");
         } else {
-        reveals[i].classList.remove("active");
+            reveals[i].classList.remove("active");
         }
     }
-    }
-    window.addEventListener("load", reveal);
-    window.addEventListener("scroll", reveal);
+}
+    
+window.addEventListener("scroll", reveal);
+
+$(document).ready(function() {
+
+    reveal();
+})
